939a8a057bbaef9aaeda62c418b1f8bb0693c48c
arthur-gal-pin <arthurrgspinto@gmail.com>
2026-05-21 09:13:27 -0300

    :sparkles: feat(api): Adicionada a função de rendereização de imagem.

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 import router from './routes/routes';
 import 'dotenv/config';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT;
 
+app.use('/uploads', express.static('uploads'));
 app.use(express.json());
+app.use(cors());
 app.use('/', router);
+
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`)
